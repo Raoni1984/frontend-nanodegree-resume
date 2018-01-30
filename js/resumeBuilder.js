ed956378640c3a4d1619ff837a581dfb8638100d
Raoni <raonico@gmail.com>
2018-01-30 13:40:14 -0200

    first commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var myName = "Raoni";
+var myRole = "Web Dev";
+
+var formattedName = HTMLheaderName.replace("%data%", myName);
+var formattedRole = HTMLheaderRole.replace("%data%", myRole);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole)
